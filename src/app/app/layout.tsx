2ba070cf4d1da14b6fbf8e6d415268f28b7bd0d1
Julius Caezar <julius.caezar25@gmail.com>
2024-03-13 15:17:46 -0300

    feat: importando a mainSideBar

---
+++ src/app/app/layout.tsx
@@ -0,0 +1,18 @@
+
+import { PropsWithChildren } from 'react'
+
+import { auth } from '@/services/auth'
+
+import { MainSidebar } from './_components/main-sidebar'
+
+export default async function Layout({ children }: PropsWithChildren) {
+  const session = await auth()
+
+  return (
+    <div className="grid grid-cols-[16rem_1fr] ">
+      <MainSidebar user={session?.user}/>
+
+      <main>{children}</main>
+    </div>
+  )
+}
