18a85bcdc40a70cd84131d1539eea43d026566f6
Julius Caezar <julius.caezar25@gmail.com>
2024-03-13 15:12:56 -0300

    feat: criando a sidebar

---
+++ src/app/app/_components/main-sidebar.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import {
+  Sidebar,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMain,
+  SidebarNav,
+  SidebarNavHeader,
+  SidebarNavHeaderTitle,
+  SidebarNavLink,
+  SidebarNavMain
+} from '@/components/dashboard/sidebar'
+import { Logo } from '@/components/logo'
+import { UserDropdown } from './user-dropdown'
+
+import { HomeIcon, MixerVerticalIcon } from '@radix-ui/react-icons'
+
+import { usePathname } from 'next/navigation'
+import { Session } from 'next-auth'
+
+type MainSidebarProps = {
+  user: Session['user']
+}
+
+export function MainSidebar({ user }: MainSidebarProps) {
+  const pathname = usePathname()
+
+  const isActive = (path: string) => {
+    return pathname === path
+  }
+
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <Logo />
+      </SidebarHeader>
+      <SidebarMain className="flex flex-col flex-grow">
+        <SidebarNav>
+          <SidebarNavMain>
+            <SidebarNavLink href="/app" active={isActive('/app')}>
+              <HomeIcon className="w-3 h-3 mr-3" />
+              Tarefas
+            </SidebarNavLink>
+            <SidebarNavLink
+              href="/app/settings"
+              active={isActive('/app/settings')}
+            >
+              <MixerVerticalIcon className="w-3 h-3 mr-3" />
+              Configurações
+            </SidebarNavLink>
+          </SidebarNavMain>
+        </SidebarNav>
+
+        <SidebarNav className="mt-auto">
+          <SidebarNavHeader>
+            <SidebarNavHeaderTitle>Links extras</SidebarNavHeaderTitle>
+          </SidebarNavHeader>
+          <SidebarNavMain>
+            <SidebarNavLink href="/">Precisa de ajuda?</SidebarNavLink>
+            <SidebarNavLink href="/">Site</SidebarNavLink>
+          </SidebarNavMain>
+        </SidebarNav>
+      </SidebarMain>
+      <SidebarFooter>
+        <UserDropdown user={user}/>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
