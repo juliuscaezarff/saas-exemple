f3b89121fbde1120f40e73683ed355501ebf2152
Julius Caezar <julius.caezar25@gmail.com>
2024-03-12 23:24:58 -0300

    feat: centralizando a box de login e add um logo

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,11 @@
-import { AuthForm } from "./_components/auth-form";
+import { Logo } from '@/components/logo'
+import { AuthForm } from './_components/auth-form'
 
 export default function Page() {
-  return <AuthForm />
+  return (
+    <main className='flex flex-col items-center justify-center h-screen gap-7'>
+      <Logo className='h-10 w-10'/>
+      <AuthForm />
+    </main>
+  )
 }
