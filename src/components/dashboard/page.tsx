6695262cd2f3125759c2739ddcf5da35b86e46b6
Julius Caezar <julius.caezar25@gmail.com>
2024-03-20 16:07:16 -0300

    feat: adjusting the indentations and styling the header

--- src/components/dashboard/page.tsx
+++ src/components/dashboard/page.tsx
@@ -5,26 +5,45 @@ export type DashboardPageGenericProps<T = any> = {
   className?: string
 } & T
 
-export function DashboardPage({ children, className }: DashboardPageGenericProps) {
+export function DashboardPage({
+  children,
+  className
+}: DashboardPageGenericProps) {
   return <section className={cn(['h-screen', className])}>{children}</section>
 }
 
-export function DashboardPageHeader({ children, className }: DashboardPageGenericProps) {
+export function DashboardPageHeader({
+  children,
+  className
+}: DashboardPageGenericProps) {
   return (
-    <header className={cn(['px-6 py-3 border-b border-border', className])}>
+    <header className={cn(['px-6 py-3 border-b border-border flex items-center justify-between', className])}>
       {children}
     </header>
   )
 }
 
-export function DashboardPageHeaderTitle({ children, className }: DashboardPageGenericProps) {
-  return <span className={cn(['text-muted-foreground uppercase', className])}>{children}</span>
+export function DashboardPageHeaderTitle({
+  children,
+  className
+}: DashboardPageGenericProps) {
+  return (
+    <span className={cn(['text-muted-foreground uppercase', className])}>
+      {children}
+    </span>
+  )
 }
 
-export function DashboardPageHeaderNav({ children, className }: DashboardPageGenericProps) {
+export function DashboardPageHeaderNav({
+  children,
+  className
+}: DashboardPageGenericProps) {
   return <nav className={cn(['', className])}>{children}</nav>
 }
 
-export function DashboardPageMain({ children, className }: DashboardPageGenericProps) {
+export function DashboardPageMain({
+  children,
+  className
+}: DashboardPageGenericProps) {
   return <main className={cn(['p-6', className])}>{children}</main>
 }
