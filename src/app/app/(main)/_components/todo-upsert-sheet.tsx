4e5cd4ebee3969dc82cff47f53648f5ce2e5b90c
Julius Caezar <julius.caezar25@gmail.com>
2024-03-20 16:24:17 -0300

    feat: add todo crud with server actions

---
+++ src/app/app/(main)/_components/todo-upsert-sheet.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from '@/components/ui/sheet'
+import { useRef } from 'react'
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage
+} from '@/components/ui/form'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Todo } from '../types'
+import { upsertTodo } from '../actions'
+import { upsertTodoSchema } from '../schema'
+import { useRouter } from 'next/navigation'
+import { toast } from '@/components/ui/use-toast'
+
+type TodoUpsertSheetProps = {
+  children?: React.ReactNode
+  defaultValue?: Todo
+}
+
+export function TodoUpsertSheet({ children }: TodoUpsertSheetProps) {
+  const ref = useRef<HTMLDivElement>(null)
+  const router = useRouter()
+  const form = useForm({
+    resolver: zodResolver(upsertTodoSchema)
+  })
+
+  const onSubmit = form.handleSubmit(async (data) => {
+    await upsertTodo(data)
+    router.refresh()
+
+    ref.current?.click()
+
+    toast({
+      title: 'Success',
+      description: 'The task has been successfully updated.',
+    })
+  })
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <div ref={ref}>{children}</div>
+      </SheetTrigger>
+      <SheetContent>
+        <Form {...form}>
+          <form onSubmit={onSubmit} className="space-y-8 h-screen">
+            <SheetHeader>
+              <SheetTitle>Upsert Todo</SheetTitle>
+              <SheetDescription>
+                Make changes to your profile here. Click save when you're done.
+              </SheetDescription>
+            </SheetHeader>
+
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter the title of your task"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    This will be the visible title of your task.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <SheetFooter>
+              <Button type="submit">Save changes</Button>
+            </SheetFooter>
+          </form>
+        </Form>
+      </SheetContent>
+    </Sheet>
+  )
+}
