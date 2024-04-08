2421be3c730a4457e373115cc966ceef99477d15
Julius Caezar <julius.caezar25@gmail.com>
2024-04-08 20:05:47 -0300

    feat: creating links to routes from profile settings

---
+++ src/app/app/settings/_components/settings-sidebar.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import {
+  SidebarNav,
+  SidebarNavLink,
+  SidebarNavMain
+} from '@/components/dashboard/sidebar'
+import { usePathname } from 'next/navigation'
+
+export function SettingsSidebar() {
+  const pathname = usePathname()
+
+  const isActive = (path: string) => {
+    return pathname === path
+  }
+
+  return (
+    <aside>
+      <SidebarNav>
+        <SidebarNavMain>
+          <SidebarNavLink
+            href="/app/settings"
+            active={isActive('/app/settings')}
+          >
+            My profile
+          </SidebarNavLink>
+          <SidebarNavLink
+            href="/app/settings/theme"
+            active={isActive('/app/settings/theme')}
+          >
+            Theme
+          </SidebarNavLink>
+          <SidebarNavLink
+            href="/app/settings/billing"
+            active={isActive('/app/settings/billing')}
+          >
+            Billing
+          </SidebarNavLink>
+        </SidebarNavMain>
+      </SidebarNav>
+    </aside>
+  )
+}
