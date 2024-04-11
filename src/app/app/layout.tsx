c7033ea8ad636fc57fb77d1db83676225d78a38a
Julius Caezar <julius.caezar25@gmail.com>
2024-04-11 18:47:08 -0300

    feat: importing the ThemeProvider and configuring

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,18 +1,25 @@
-
 import { PropsWithChildren } from 'react'
 
 import { auth } from '@/services/auth'
 
 import { MainSidebar } from './_components/main-sidebar'
+import { ThemeProvider } from '../_components/theme-provider'
 
 export default async function Layout({ children }: PropsWithChildren) {
   const session = await auth()
 
   return (
     <div className="grid grid-cols-[16rem_1fr] ">
-      <MainSidebar user={session?.user}/>
+      <MainSidebar user={session?.user} />
 
-      <main>{children}</main>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
     </div>
   )
 }
