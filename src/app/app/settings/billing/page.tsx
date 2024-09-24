80c19dc06374ccda047d99cbaed333c7740529eb
Julius Caezar <julius.caezar25@gmail.com>
2024-09-24 16:42:48 -0300

    feat: Implementa exibição do plano atual do usuário, Mostra progresso de uso das tarefas (quota), Integra com ação de criação de sessão de checkout

--- src/app/app/settings/billing/page.tsx
+++ src/app/app/settings/billing/page.tsx
@@ -5,36 +5,45 @@ import {
   CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle
+  CardTitle,
 } from '@/components/ui/card'
 import { Progress } from '@/components/ui/progress'
 import { createCheckoutSessionAction } from './actions'
+import { auth } from '@/services/auth'
+import { getUserCurrentPlan } from '@/services/stripe'
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth()
+  const plan = await getUserCurrentPlan(session?.user.id as string)
+
   return (
-    <form action={createCheckoutSessionAction} >
+    <form action={createCheckoutSessionAction}>
       <Card>
         <CardHeader className="border-b border-border">
           <CardTitle>Uso do Plano</CardTitle>
           <CardDescription>
-            Você está atualmente no plano [current_plan]. Ciclo de faturamento
-            atual: Março [next_due_date].
+            Você está atualmente no{' '}
+            <span className="font-bold uppercase">{plan.name}</span>.
           </CardDescription>
         </CardHeader>
         <CardContent className="pt-6">
           <div className="space-y-2">
             <header className="flex items-center justify-between">
-              <span className="text-muted-foreground text-sm">1/5</span>
-              <span className="text-muted-foreground text-sm">20%</span>
+              <span className="text-muted-foreground text-sm">
+                {plan.quota.TASKS.current}/{plan.quota.TASKS.available}
+              </span>
+              <span className="text-muted-foreground text-sm">
+                {plan.quota.TASKS.usage}%
+              </span>
             </header>
             <main>
-              <Progress value={20} />
+              <Progress value={plan.quota.TASKS.usage} />
             </main>
           </div>
         </CardContent>
         <CardFooter className="flex items-center justify-between border-t border-border pt-6">
-          <span>Para maior limite assine o PRO</span>
-          <Button type='submit'>Assine por R$ 9/ mês</Button>
+          <span>Para um maior limite, assine o PRO</span>
+          <Button type="submit">Assine por R$9/ mês</Button>
         </CardFooter>
       </Card>
     </form>
