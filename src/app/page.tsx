6850fde2577c2957031f0404d95035aaa542b6a8
Julius Caezar <julius.caezar25@gmail.com>
2024-10-08 16:06:09 -0300

    feat: importando os components da lp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import Benefits from "@/components/home/benefits";
+import FAQSection from "@/components/home/faq";
+import HeroSection from "@/components/home/hero";
+import Join from "@/components/home/join";
+import Price from "@/components/home/price";
+import Footer from "@/components/ui/footer";
+import Header from "@/components/ui/header";
 
 
 export default function Home() {
   return (
    <main>
-    Lading page
+    <Header />
+    <HeroSection />
+    <Benefits />
+    <Price />
+    <FAQSection />
+    <Join />
+    <Footer />
    </main>
   );
 }
